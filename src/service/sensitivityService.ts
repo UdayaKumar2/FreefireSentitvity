476de601b0d242d446589186e24dd1145919940a
UdayaKumar P <147427596+UdayaKumar2@users.noreply.github.com>
2025-06-14 14:31:30 +0530

    Delete src directory

--- src/service/sensitivityService.ts
+++
@@ -1,9 +0,0 @@
-export default function getSensitivitySettings(baseSensitivity: number): number {
-  const min = baseSensitivity;
-  const max = Math.min(baseSensitivity + 5, 200);
-
-  const adjustedSensitivity = Math.round(Math.random() * (max - min) + min);
-
-  return adjustedSensitivity;
-  
-}
